28ec916ecd6674368d989249787a946ad8a6667a
Wes Bunton <wesley.bunton@gmail.com>
2016-12-23 22:03:53 -0500

    Switched target SDK to Marshmellow. Add an icon file left out of last commit.

--- app/src/main/java/com/wesbunton/projects/mycertificates/MainActivity.java
+++ app/src/main/java/com/wesbunton/projects/mycertificates/MainActivity.java
@@ -122,6 +122,9 @@ public class MainActivity extends AppCompatActivity {
             alertDialog.setPositiveButton("OK", null);
             alertDialog.setMessage("Go to wesbunton.github.io more info");
 
+            // TODO - Add reference for free icon
+            // <div>Icons made by <a href="http://www.flaticon.com/authors/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
             // Show the alert
             AlertDialog alert = alertDialog.create();
             alert.show();
